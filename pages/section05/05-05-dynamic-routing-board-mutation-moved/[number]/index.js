e4fed54842d0ffe18afad1b62ea04e3b43a68716
sonwooheon <b29299461@gmail.com>
2024-04-10 21:09:36 +0900

    test

--- pages/section05/05-05-dynamic-routing-board-mutation-moved/[number]/index.js
+++ pages/section05/05-05-dynamic-routing-board-mutation-moved/[number]/index.js
@@ -20,7 +20,7 @@ export default function StaticRoutingMovedPage(){
     console.log(data)
     return(
         <div>
-            <div>{router.query.number}번 게시글 이동이 완료되었습니다.</div>
+            <div>{router.query.number}번 게시글 이동이 완료되었습니다.tset</div>
             <div>작성자 : {data?.fetchBoard?.writer}</div>
             <div>제목 : {data?.fetchBoard?.title}</div>
             <div>내용 : {data?.fetchBoard?.contents}</div>
