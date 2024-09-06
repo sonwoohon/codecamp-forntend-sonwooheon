ae534dacac498a12fccbd72dd78fd64e98ee95d8
Son Woo Heon <b29299461@gmail.com>
2024-09-06 17:45:33 +0900

    10-02 다시하기

---
+++ pages/section10/09-04-boards/[number]/edit/index.js
@@ -0,0 +1,32 @@
+
+import BoardWrite from "../../../../../src/components/units/board/10-write/BoardWrite.container"
+import {useQuery,gql} from '@apollo/client'
+import { useRouter } from 'next/router'
+
+const FETCH_BOARD = gql`
+query fetchBoard($number:Int){
+    fetchBoard(number:$number){
+        number
+        writer
+        title
+        contents
+    }
+}
+`
+
+export default function GraphqlMutationPage(){
+    const router = useRouter()
+    const { data } = useQuery(FETCH_BOARD, {
+        variables:{ number:Number(router.query.number) }
+    })
+
+    return(
+        <div>
+            <div>#########여기페이지########</div>
+            <BoardWrite isEdit={true} data={data} />
+            <div>#########여기페이지########</div>
+        </div>
+    ) 
+    
+
+}
---
+++ src/components/units/board/10-write/BoardWrite.presenter.js
@@ -0,0 +1,17 @@
+import { RedInput, BlueInput } from "./BoardWrite.styles"
+
+export default function BoardWriteUI (props){
+
+    return (
+        <div>
+            <div>#########여기 프리센터########</div>
+            작성자: <RedInput type='text' onChange={props.onChangeWrite} defaultValue={props.data?.fetchBoard.writer} />
+            제목: <input type='text' onChange={props.onChangeTitle} defaultValue={props.data?.fetchBoard.title} />
+            내용: <input type='text' onChange={props.onChangeContents} defaultValue={props.data?.fetchBoard.contents} />
+            <BlueInput onClick={props.isEdit? props.onClickUpdate : props.onClickSubmit}>
+                {props.isEdit? "수정" : "등록"}하기
+            </BlueInput>
+            <div>#########여기 프리센터########</div>
+        </div>
+    )
+}
